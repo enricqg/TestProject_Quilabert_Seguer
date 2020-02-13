af749733e29bdd6706d91f7bb9d60f4211ac3dd8
David Seguer Pes <david.seguer@enti.cat>
2020-02-13 12:18:55 +0100

    he posat un for

--- enc_temp_folder/184cef476defa6a0d77d9c95ca38a185/Source.cpp
+++ enc_temp_folder/184cef476defa6a0d77d9c95ca38a185/Source.cpp
@@ -3,7 +3,7 @@
 void main() {
 	std::cout << "Hello Enric" << std::endl;
 
-	for (int a = 0; a < 100; a++) {
+	for (int a = 0; a < 10; a++) {
 
 		std::cout << "HOLA SOC EN BOLA UNA RICA GOMINOLA"<< std::endl;
 	}
